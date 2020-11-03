5cb1486a8631654f07e8d67e5c7f11d1638c3377
sunxmaz <sunxmaz@163.com>
2020-11-03 10:14:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const http=require('http')
+let users=[
+    {id:1,name:'zhufeng1'},
+    {id:2,name:'zhufeng2'}
+]
+let server=http.createServer((req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    if(req.url==='/api/users'){
+        res.end(JSON.stringify(users))
+    }else{
+        res.end('Not found')
+    }
+})
+server.listen(3000,()=>{
+    console.log('api start in 3000')
+})
