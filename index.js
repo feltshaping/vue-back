0d8ed155cd69306461f35ef9fec70a868f7dabc6
sunxm <sunxmaz@163.com>
2021-06-04 10:09:58 +0800

    change users

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const http=require('http')
-let users=[
-    {id:1,name:'zhufeng1'},
-    {id:2,name:'zhufeng2'},
-    {id:3,name:'sunxmaz'},
-    {id:4,name:'sunxmaz1'},
-    {id:5,name:'sunxmaz2'}
-
-
-
-]
-let server=http.createServer((req,res)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
-    if(req.url==='/api/users'){
-        res.end(JSON.stringify(users))
-    }else{
-        res.end('Not found')
-    }
-})
-server.listen(3000,()=>{
-    console.log('api start in 3000')
-})
+const http = require('http');
+let users = [
+  { id: 1, name: 'sunxm' },
+  { id: 2, name: 'zhufeng2' },
+  { id: 3, name: 'sunxmaz' },
+  { id: 4, name: 'sunxmaz1' },
+  { id: 5, name: 'sunxmaz2' },
+];
+let server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.url === '/api/users') {
+    res.end(JSON.stringify(users));
+  } else {
+    res.end('Not found');
+  }
+});
+server.listen(3000, () => {
+  console.log('api start in 3000');
+});
