80ac6041e5fd2a61f2a800fbffd6335f87c7b54d
sunxm <sunxmaz@163.com>
2023-09-01 16:20:14 +0800

    again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-let users = [{ id: 0, name: 'finally' },{ id: 1, name: 'again' },{id:2,name:'sunxmaz'},{id:3,name:'zhang'}];
+let users = [{ id: 0, name: 'finally' },{ id: 1, name: 'again' },{id:2,name:'sunxmaz'},{id:3,name:'zhangxw'}];
 let server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   if (req.url === '/api/users') {
