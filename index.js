c26fd4cb2462562a532d55984083e5349410e764
sunxm <sunxmaz@163.com>
2023-09-01 16:23:37 +0800

    gingko

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-let users = [{ id: 0, name: 'finally' },{ id: 1, name: 'again' },{id:2,name:'sunxmaz'},{id:3,name:'zhangxw'}];
+let users = [{ id: 0, name: 'finally' },{ id: 1, name: 'again' },{id:2,name:'sunxmaz'},{id:3,name:'zhangxw'},{id:4,name:'gingko'}];
 let server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   if (req.url === '/api/users') {
